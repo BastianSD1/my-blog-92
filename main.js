d8eff00b72061a5f05740344973f41278c922261
BastianSD1 <139516022+BastianSD1@users.noreply.github.com>
2023-11-18 10:29:07 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+var canvas = new fabric.Canvas('myCanvas');
+var x= document.getElementById("myAudio");
+
+function new_image()
+fabric.image.fromURL('birthdayimage.jpg', function(img)
+block_image_object= Img;
+block_image_object.scaletowidth(700);
+block_image_object.scaletoheight(510);
+block_image_object.set{(
+top=0,
+left=0
+)};
+canvas.add(block_image-object);
+{
+
+
+
+
+
+	
+
+
+}
+
+function playSound(){
+x.play();
+}
